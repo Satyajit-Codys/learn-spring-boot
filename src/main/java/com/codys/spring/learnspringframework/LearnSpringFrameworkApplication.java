66a29b1ac47612e5be0c47bad5fc3241988ce77c
Satya-Codys <satyajitdebnath87@gmail.com>
2023-05-09 09:19:46 +0530

    mario game class and constructor

--- src/main/java/com/codys/spring/learnspringframework/LearnSpringFrameworkApplication.java
+++ src/main/java/com/codys/spring/learnspringframework/LearnSpringFrameworkApplication.java
@@ -3,11 +3,17 @@ package com.codys.spring.learnspringframework;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.codys.spring.learnspringframework.game.GameRunner;
+import com.codys.spring.learnspringframework.game.MarioGame;
+
 @SpringBootApplication
 public class LearnSpringFrameworkApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LearnSpringFrameworkApplication.class, args);
+		//SpringApplication.run(LearnSpringFrameworkApplication.class, args);
+		MarioGame game = new MarioGame();
+		GameRunner runner = new GameRunner(game);
+		runner.run();
 	}
 
 }
---
+++ src/main/java/com/codys/spring/learnspringframework/game/GameRunner.java
@@ -0,0 +1,20 @@
+package com.codys.spring.learnspringframework.game;
+
+public class GameRunner {
+	
+	private MarioGame game;
+	
+	public GameRunner(MarioGame game) {
+		this.game = game;
+
+	}
+
+	public void run() {
+		game.up();
+		game.down();
+		game.left();
+		game.right();
+		
+	}
+
+}
