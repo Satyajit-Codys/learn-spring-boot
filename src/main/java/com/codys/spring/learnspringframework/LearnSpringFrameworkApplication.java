10e4ffd0ddcb91454d2f4974050799205718e42a
Satya-Codys <satyajitdebnath87@gmail.com>
2023-05-09 09:35:40 +0530

    loose coupling using interface

--- src/main/java/com/codys/spring/learnspringframework/LearnSpringFrameworkApplication.java
+++ src/main/java/com/codys/spring/learnspringframework/LearnSpringFrameworkApplication.java
@@ -5,13 +5,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.codys.spring.learnspringframework.game.GameRunner;
 import com.codys.spring.learnspringframework.game.MarioGame;
+import com.codys.spring.learnspringframework.game.SuperContraGame;
 
 @SpringBootApplication
 public class LearnSpringFrameworkApplication {
 
 	public static void main(String[] args) {
 		//SpringApplication.run(LearnSpringFrameworkApplication.class, args);
-		MarioGame game = new MarioGame();
+//		MarioGame game = new MarioGame();
+		SuperContraGame game = new SuperContraGame();
 		GameRunner runner = new GameRunner(game);
 		runner.run();
 	}
--- src/main/java/com/codys/spring/learnspringframework/game/MarioGame.java
+++ src/main/java/com/codys/spring/learnspringframework/game/MarioGame.java
@@ -1,6 +1,6 @@
 package com.codys.spring.learnspringframework.game;
 
-public class MarioGame {
+public class MarioGame implements GamingConsole {
 	
 	public void up() {
 		System.out.println("up");
