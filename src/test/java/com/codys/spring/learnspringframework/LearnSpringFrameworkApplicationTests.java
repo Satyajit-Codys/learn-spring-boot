d7ad9d6e672a71ddde4ae74911ec6974f9c2f0ff
Debnath <2144048@cognizant.com>
2023-05-09 08:28:26 +0530

    initial commit

---
+++ src/test/java/com/codys/spring/learnspringframework/LearnSpringFrameworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codys.spring.learnspringframework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnSpringFrameworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
